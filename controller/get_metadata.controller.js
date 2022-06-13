faa1ec3a955a246b3e8f4acb76d0411f4c4cc329
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-14 00:57:49 +0800

    view etadata

--- controller/get_metadata.controller.js
+++ controller/get_metadata.controller.js
@@ -11,9 +11,21 @@ const { getMetadatas, createMetadata } = useMetadata()
 const { player_login, player_logout, player_register, getPlayer } = usePlayer()
 const { fetchLatestTransaction } = useBigchaindb()
 // api/products
-router.get('/get_metadata', async (req, res) => {
+router.get('/get_metadata/:id', async (req, res) => {
     try {
-        res.status(200).json("hello")
+
+        const assetId = req.params.id
+
+        const latestFetchMeta = await fetchLatestTransaction(assetId)
+
+        res.status(200).json({
+            name: latestFetchMeta.metadata.name,
+            description: latestFetchMeta.metadata.description,
+            tokenId: latestFetchMeta.metadata.token_id,
+            attributes: latestFetchMeta.metadata.attributes,
+            image: latestFetchMeta.metadata.image,
+            "current-chain": latestFetchMeta.metadata.current_chain
+        })
     } catch (error) {
         res.status(400).json(error)
     }
